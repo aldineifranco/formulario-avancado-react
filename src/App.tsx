fa8008d1d8642918059657d32f30ff2de04ebb4a
Aldinei <aldinei.franco@unimedprudente.com.br>
2023-04-06 10:04:01 -0300

    dark theme

--- src/App.tsx
+++ src/App.tsx
@@ -10,23 +10,23 @@ export function App() {
   }
 
   return (
-    <main className='h-screen bg-zinc-100 flex items-center justify-center flex-col gap-8'>
+    <main className='h-screen flex items-center justify-center flex-col gap-8 bg-slate-900'>
       <form
         onSubmit={handleSubmit(createUser)}
-        className='flex flex-col gap-6 w-full max-w-xs'>
+        className='flex flex-col gap-6 w-full max-w-xs bg-slate-800 px-4 py-8 rounded text-white'>
 
         <div className='flex flex-col gap-1'>
           <label htmlFor="">E-mail</label>
           <input
             type="email"
-            className="border border-zinc-200 shadow-sm rounded h-10 px-3" {...register('email')} />
+            className="border border-zinc-200 shadow-sm rounded h-10 px-3 text-slate-600" {...register('email')} />
         </div>
 
         <div className='flex flex-col gap-1'>
           <label htmlFor="">Senha</label>
           <input
             type="password"
-            className="border border-zinc-200 shadow-sm rounded h-10 px-3" {...register('password')} />
+            className="border border-zinc-200 shadow-sm rounded h-10 px-3 text-slate-600" {...register('password')} />
         </div>
 
         <button
@@ -37,7 +37,9 @@ export function App() {
         </button>
 
       </form>
-      {output} - parei em 25:00
+      <div className='text-white px-10 text-center'>
+        {output} 
+         - parei em 25:00</div>
     </main>
   )
 }
